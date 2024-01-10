e02ac56b6629f9e30ecbc3217f8ba09959aa5462
samanthacsik <samanthacsik@gmail.com>
2024-01-10 11:29:56 -0800

    polished up 4.3 slides

--- assign-learning-partners.R
+++ assign-learning-partners.R
@@ -35,5 +35,5 @@ student_groups <- cbind(students, group_num) |>
   arrange(group_num)
 
 #..........................write out csv.........................
-write_csv(x = student_groups, file = here::here("student_groups", "week1_learning_partners.csv"))
+# write_csv(x = student_groups, file = here::here("student_groups", "week3_learning_partners.csv"))
 
